b9308aaa06de79ecb466af191dc0aa5ffbe89456
Timo <619373154@qq.com>
2018-03-12 23:56:30 +0800

    a

--- C++/pro/main0302-2.cpp
+++ C++/pro/main0302-2.cpp
@@ -1,45 +1,39 @@
-#include <iostream>
-using namespace std; 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int qqnum[5000];
-int kKnum=0;
-int find_high(int *num,int a,int b)
-{
-	int highest=0;
-	for(int i=a+1;i<=b;i++)
-	{
-		if(num[i]>highest)
-			highest=num[i];
-	}
-	qqnum[kKnum++]=highest;
-	return highest;
+
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+ 
+const int MAXN=100000;
+ 
+int data[MAXN+5];
+ 
+int querymax ( int l , int r ) {
+    int ans=data[l];
+    for(int i=l+1;i<=r;i++) ans=max(ans,data[i]);
+    return ans;
 }
-void update(int *num,int a,int b)
-{
-	num[a]=b;
+ 
+void update(int idx,int value){
+    data[idx]=value;
 }
-int main(int argc, char** argv) {
-	int n,m;
-	cin>>n>>m;
-	int *num=new int [n+1];//≥ı º≥…º® 
-	for(int i=1;i<=n;i++)
-	{
-		cin>>num[i];
-	}
-	for(int i=0;i<m;i++)
-	{
-		char ch;
-		cin>>ch;
-		int num1,num2;
-		cin>>num1>>num2;
-		if(ch=='Q')
-			find_high(num,num1,num2);
-		else if(ch=='U')
-			update(num,num1,num2);
-	} 
-	for(int i=0;i<kKnum;i++)
-	{
-		cout<<qqnum[i]<<endl;
-	}
-	return 0;
+ 
+int main(){
+    int n,m;
+    while(~scanf("%d%d",&n,&m)){
+        for(int i=1;i<=n;i++){
+            scanf("%d",&data[i]);
+        }
+        char order;
+        int a,b;
+        for(;m--;){
+            scanf(" %c%d%d",&order,&a,&b);
+            if(order=='U'){
+                update(a,b);
+            }else if(order=='Q'){
+                if(a>b)swap(a,b);
+                printf("%d\n",querymax(a,b));
+            }
+        }
+    }
+    return 0;
 }

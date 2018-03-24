fb357c3fa11bbd1ea2fcd36926a81ad7faaafa7e
Timo <619373154@qq.com>
2018-03-24 18:44:43 +0800

    test

---
+++ C++/pro/main0324-2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int a=0x80000000;
+	int b=0x7fffffff;
+	cout<<"a="<<a<<"b="<<b<<endl<<a+b<<endl;
+	return 0;
+}

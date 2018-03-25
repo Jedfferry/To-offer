22de97fab8938c8815543780ef27411a1b6b0300
Timo <619373154@qq.com>
2018-03-25 19:10:21 +0800

    头条笔试题

---
+++ C++/pro/main0324-6.cpp
@@ -0,0 +1,379 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+void fun11(){	cout<<"....6";}
+void fun12(){	cout<<"....6";}
+void fun13(){	cout<<"....6";}
+void fun14(){	cout<<"....6";}
+void fun15(){	cout<<"....6";}
+
+void fun21(){	cout<<"66666";}
+void fun22(){	cout<<"....6";}
+void fun23(){	cout<<"66666";}
+void fun24(){	cout<<"6....";}
+void fun25(){	cout<<"66666";}
+
+void fun31(){	cout<<"66666";}
+void fun32(){	cout<<"....6";}
+void fun33(){	cout<<"66666";}
+void fun34(){	cout<<"....6";}
+void fun35(){	cout<<"66666";}
+
+void fun41(){	cout<<"6...6";}
+void fun42(){	cout<<"6...6";}
+void fun43(){	cout<<"66666";}
+void fun44(){	cout<<"....6";}
+void fun45(){	cout<<"....6";}
+
+void fun51(){	cout<<"66666";}
+void fun52(){	cout<<"6....";}
+void fun53(){	cout<<"66666";}
+void fun54(){	cout<<"....6";}
+void fun55(){	cout<<"66666";}
+
+void fun61(){	cout<<"66666";}
+void fun62(){	cout<<"6....";}
+void fun63(){	cout<<"66666";}
+void fun64(){	cout<<"6...6";}
+void fun65(){	cout<<"66666";}
+
+void fun71(){	cout<<"66666";}
+void fun72(){	cout<<"....6";}
+void fun73(){	cout<<"....6";}
+void fun74(){	cout<<"....6";}
+void fun75(){	cout<<"....6";}
+
+void fun81(){	cout<<"66666";}
+void fun82(){	cout<<"6...6";}
+void fun83(){	cout<<"66666";}
+void fun84(){	cout<<"6...6";}
+void fun85(){	cout<<"66666";}
+
+void fun91(){	cout<<"66666";}
+void fun92(){	cout<<"6...6";}
+void fun93(){	cout<<"66666";}
+void fun94(){	cout<<"....6";}
+void fun95(){	cout<<"....6";}
+
+void fun01(){	cout<<"66666";}
+void fun02(){	cout<<"6...6";}
+void fun03(){	cout<<"6...6";}
+void fun04(){	cout<<"6...6";}
+void fun05(){	cout<<"66666";}
+void display(int *a,int count)
+{
+	int ss0=1,ss1=1,ss2=1,ss3=1,ss4=1,ss5=1;;
+	for(int i=0;i<count;i++)
+	{
+		switch(a[i])
+		{
+			case 0:fun01();if(ss1<count) {
+				cout<<"..";ss1++;
+			
+			}	break;
+			case 1:fun11();if(ss1<count) {
+				cout<<"..";ss1++;
+			
+			}	break;
+			case 2:fun21();if(ss1<count){
+				cout<<"..";ss1++;
+			
+			}	break;
+			case 3:fun31();if(ss1<count) {
+				cout<<"..";ss1++;
+			
+			}	break;
+			case 4:fun41();if(ss1<count) {
+				cout<<"..";ss1++;
+			
+			}	break;
+			case 5:fun51();if(ss1<count) {
+				cout<<"..";ss1++;
+			
+			}	break;
+			case 6:fun61();if(ss1<count){
+				cout<<"..";ss1++;
+			
+			}	break;
+			case 7:fun71();if(ss1<count){
+				cout<<"..";ss1++;
+			
+			}	break;
+			case 8:fun81();if(ss1<count) {
+				cout<<"..";ss1++;
+			
+			}	break;
+			case 9:fun91();if(ss1<count) {
+				cout<<"..";ss1++;
+			
+			}	break;
+		}
+	}
+	cout<<endl;
+	for(int i=0;i<count;i++)
+	{
+		switch(a[i])
+		{
+			case 0:fun02();if(ss2<count) {
+				cout<<"..";ss2++;
+			
+			}	break;
+			case 1:fun12();if(ss2<count) {
+				cout<<"..";ss2++;
+			
+			}	break;
+			case 2:fun22();if(ss2<count) {
+				cout<<"..";ss2++;
+			
+			}	break;
+			case 3:fun32();if(ss2<count) {
+				cout<<"..";ss2++;
+			
+			}	break;
+			case 4:fun42();if(ss2<count) {
+				cout<<"..";ss2++;
+			
+			}	break;
+			case 5:fun52();if(ss2<count) {
+				cout<<"..";ss2++;
+			
+			}	break;
+			case 6:fun62();if(ss2<count) {
+				cout<<"..";ss2++;
+			
+			}	break;
+			case 7:fun72();if(ss2<count) {
+				cout<<"..";ss2++;
+			
+			}	break;
+			case 8:fun82();if(ss2<count) {
+				cout<<"..";ss2++;
+			
+			}	break;
+			case 9:fun92();if(ss2<count) {
+				cout<<"..";ss2++;
+			
+			}	break;
+		}
+	}
+	cout<<endl;
+	for(int i=0;i<count;i++)
+	{
+		switch(a[i])
+		{
+			case 0:fun03();if(ss3<count) {
+				cout<<"..";ss3++;
+			
+			}	break;
+			case 1:fun13();if(ss3<count) {
+				cout<<"..";ss3++;
+			
+			}	break;
+			case 2:fun23();if(ss3<count) {
+				cout<<"..";ss3++;
+			
+			}	break;
+			case 3:fun33();if(ss3<count) {
+				cout<<"..";ss3++;
+			
+			}	break;
+			case 4:fun43();if(ss3<count) {
+				cout<<"..";ss3++;
+			
+			}	break;
+			case 5:fun53();if(ss3<count) {
+				cout<<"..";ss3++;
+			
+			}	break;
+			case 6:fun63();if(ss3<count) {
+				cout<<"..";ss3++;
+			
+			}	break;
+			case 7:fun73();if(ss3<count) {
+				cout<<"..";ss3++;
+			
+			}	break;
+			case 8:fun83();if(ss3<count) {
+				cout<<"..";ss3++;
+			
+			}	break;
+			case 9:fun93();if(ss3<count) {
+				cout<<"..";ss3++;
+			
+			}	break;
+			
+		}
+	}
+	cout<<endl;
+	for(int i=0;i<count;i++)
+	{
+		switch(a[i])
+		{
+			case 0:fun04();if(ss3<count) {
+				cout<<"..";ss3++;
+			
+			}	break;
+			case 1:fun14();if(ss4<count) {
+				cout<<"..";ss4++;
+			
+			}	break;
+			case 2:fun24();if(ss4<count) {
+				cout<<"..";ss4++;
+			
+			}	break;
+			case 3:fun34();if(ss4<count) {
+				cout<<"..";ss4++;
+			
+			}	break;
+			case 4:fun44();if(ss4<count) {
+				cout<<"..";ss4++;
+			
+			}	break;
+			case 5:fun54();if(ss4<count) {
+				cout<<"..";ss4++;
+			
+			}	break;
+			case 6:fun64();if(ss4<count) {
+				cout<<"..";ss4++;
+			
+			}	break;
+			case 7:fun74();if(ss4<count) {
+				cout<<"..";ss4++;
+			
+			}	break;
+			case 8:fun84();if(ss4<count) {
+				cout<<"..";ss4++;
+			
+			}	break;
+			case 9:fun94();if(ss4<count) {
+				cout<<"..";ss4++;
+			
+			}	break;
+			
+		}
+	}
+	cout<<endl;
+	for(int i=0;i<count;i++)
+	{
+		switch(a[i])
+		{
+			case 0:fun05();if(ss3<count) {
+				cout<<"..";ss3++;
+			
+			}	break;
+			case 1:fun15();if(ss5<count) {
+				cout<<"..";ss5++;
+			
+			}	break;
+			case 2:fun25();if(ss5<count) {
+				cout<<"..";ss5++;
+			
+			}	break;
+			case 3:fun35();if(ss5<count) {
+				cout<<"..";ss5++;
+			
+			}	break;
+			case 4:fun45();if(ss5<count) {
+				cout<<"..";ss5++;
+			
+			}	break;
+			case 5:fun55();if(ss5<count) {
+				cout<<"..";ss5++;
+			
+			}	break;
+			case 6:fun65();if(ss5<count) {
+				cout<<"..";ss5++;
+			
+			}	break;
+			case 7:fun75();if(ss5<count) {
+				cout<<"..";ss5++;
+			
+			}	break;
+			case 8:fun85();if(ss5<count) {
+				cout<<"..";ss5++;
+			
+			}	break;
+			case 9:fun95();if(ss5<count) {
+				cout<<"..";ss5++;
+			
+			}	break;
+			
+
+		}
+	}
+	cout<<endl;
+}
+void show(int num)
+{
+	int linshi=num;
+	int count=0;
+	do{
+		linshi/=10;count++;
+	}while(linshi);
+	int *sum=new int [count];
+	int s_count=count;
+	linshi=num;
+	do{
+		count--;
+		sum[count]=linshi%10;
+		linshi/=10;
+	}while(linshi);
+	display(sum,s_count);
+}
+int yunsuan(int num1,int num2,int flag)
+{
+	int sum;
+	if(flag==1)
+	{
+		sum=num1+num2;
+	}
+	else if(flag==1)
+	{
+		sum=num1-num2;
+	}
+	else if(flag==3)
+	{
+		sum=num1*num2;
+	}
+	return sum;
+}
+void fun(char *ch)
+{
+	char *p=ch;
+	int length=0,num1=0,num2=0,fflag=0,flag;
+	do{
+		if((*p<'0'||*p>'9')&&(fflag==0))//第一个数 
+		{
+			if(*p=='+')
+				flag=1;//+
+			else if(*p=='+')
+			    flag=2;//-
+			else if(*p=='*')
+				flag=3;//*
+			fflag=1;p++;continue;
+		}
+		if(fflag==0)
+		{
+			num1=num1*10+(int)(*p-'0');
+		}
+		else if(fflag==1)
+		{
+			num2=num2*10+(int)(*p-'0');
+		}
+		p++;
+	}while(*p!='\0');
+	int sum=yunsuan(num1,num2,flag);
+	show(sum);
+}
+int main(int argc, char** argv) {
+	int data_num;
+	cin>>data_num;
+	char *ch=new char [data_num]; 
+	for(int i=0;i<data_num;i++)
+	{
+		cin>>ch;
+		fun(ch);
+	}
+	return 0;
+}

d5925d5a6504984b4e6f69508b9988e6b848b6da
Timo <619373154@qq.com>
2018-05-11 17:03:27 +0800

    commit

--- C++/pro/main05-11-6.cpp
+++ C++/pro/main05-11-6.cpp
@@ -3,5 +3,10 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
+	char ptr[5000];
+	while(gets(ptr))
+	{
+		char *p=ptr;
+	}
 	return 0;
 }

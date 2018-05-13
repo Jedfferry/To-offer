cc6c7d077bd74c750a2421ecd2dc8351f49fc48f
Timo <619373154@qq.com>
2018-05-13 13:20:12 +0800

    commit

--- C++/pro/main0513-1.cpp
+++ C++/pro/main0513-1.cpp
@@ -3,5 +3,29 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
+	char ch[1000];
+	while(cin>>ch)
+	{
+		int *num=new int [128];
+		for(int i=0;i<128;i++)
+		{
+			num[i]=0;
+		}
+		char *p=ch;
+		int sum=0;
+		do{
+			int n=(int)(*p);
+			if(0<=n&&n<=127)
+			{
+				if(num[n]==0)
+				{
+					num[n]=1;
+					sum++;
+				}
+			}
+			*p++;
+		}while(*p!='\0');
+		cout<<sum<<endl;
+	}
 	return 0;
 }

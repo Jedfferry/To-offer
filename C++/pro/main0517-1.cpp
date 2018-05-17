4eb8be6d97e4ff9979aae4c8283adf712ed85b83
Timo <619373154@qq.com>
2018-05-17 21:43:58 +0800

    commit

--- C++/pro/main0517-1.cpp
+++ C++/pro/main0517-1.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <string.h>
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
+	int num;
+	while(cin>>num)
+	{
+		char **ch=new char *[num];
+		for(int i=0;i<num;i++)
+		{
+			ch[i]=new char [5000];
+		}
+		for(int i=0;i<num;i++)
+		{
+			cin>>ch[i];
+		}
+	}
 	return 0;
 }

85bc8df46881fd094e2a55cf45fd20caad685077
Jedfferry <619373154@qq.com>
2018-09-14 23:22:45 +0800

    c
    1

---
+++ C++/pro/main0912-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int num1,num2;
+	while(cin>>num1>>num2)
+	{
+		int *a=new int [17]
+	}
+	return 0;
+}

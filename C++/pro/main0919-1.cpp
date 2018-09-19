52621c6505d8e13201322836e2998cb62a7609f7
Jedfferry <619373154@qq.com>
2018-09-19 16:32:02 +0800

    commit
    commiy

---
+++ C++/pro/main0919-1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int grade,score;
+	int *g=new int [32767];
+	int *s=new int [32767];
+	while(cin>>grade>>score)
+	{
+		char s1,s2;
+		s1=getchar();
+		s2=getchar();
+		if(s2=='\n')
+			break;
+	}
+	return 0;
+}
